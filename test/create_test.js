a90b39870f57c91d2c528278a49ff6695faa6255
Marcus Reis <marcus.reis@askblue.pt>
2023-08-29 23:54:17 +0100

    Automating Tests with Nodemon

--- test/create_test.js
+++ test/create_test.js
@@ -1,15 +1,14 @@
-const assert = require('assert');
-const User = require('../src/user');
+const assert = require('assert')
+const User = require('../src/user')
 
 describe('Creating records', () => {
   it('saves a user', (done) => {
-    const joe = new User({ name: 'Joe' });
+    const joe = new User({ name: 'Joe' })
 
-    joe.save()
-      .then(() => {
-        // Has joe been saved successfully?
-        assert(!joe.isNew);
-        done();
-      });
-  });
-});
+    joe.save().then(() => {
+      // Has joe been saved successfully?
+      assert(!joe.isNew)
+      done()
+    })
+  })
+})

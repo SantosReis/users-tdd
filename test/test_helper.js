ba2aa5cb5ab85805cfd9c4cc915de93e0372703a
Marcus Reis <marcus.reis@askblue.pt>
2023-08-29 22:28:46 +0100

    Default Promise Implementation

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,17 +2,23 @@ require('dotenv').config()
 require('colors')
 
 const mongoose = require('mongoose')
+mongoose.Promise = global.Promise //Force ES6 Promise style
 
-mongoose.connect(process.env.MONGO_URI)
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', (error) => {
-    console.warn('Warning', error)
-  })
+//Test only one time
+before((done) => {
+  mongoose.connect(process.env.MONGO_URI)
+  mongoose.connection
+    .once('open', () => {
+      done()
+    })
+    .on('error', (error) => {
+      console.warn('Warning', error)
+    })
+})
 
 //Reset database statements request from tests (mockups)
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
-    done();
-  });
-});
+    done()
+  })
+})

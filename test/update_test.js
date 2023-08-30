d12e45f9b569fda349e7a8d4681969815e3f7c60
Marcus Reis <marcus.reis@askblue.pt>
2023-08-30 19:29:41 +0100

    Model Instance Updates

--- test/update_test.js
+++ test/update_test.js
@@ -9,19 +9,24 @@ describe('Updating records', () => {
     joe.save().then(() => done())
   })
 
-  it('instance type using set n save', (done) => {
-    joe.set('name', 'Alex')
-    joe
-      .save()
+  function assertName(operation, done) {
+    operation
       .then(() => User.find({}))
       .then((users) => {
         assert(users.length === 1)
         assert(users[0].name === 'Alex')
         done()
       })
+  }
+
+  it('instance type using set n save', (done) => {
+    joe.set('name', 'Alex')
+    assertName(joe.save(), done)
   })
 
-  it('A model instance can update', () => {})
+  it('A model instance can update', (done) => {
+    assertName(joe.updateOne({ name: 'Alex' }), done)
+  })
 
   it('A model class can update', () => {})
 

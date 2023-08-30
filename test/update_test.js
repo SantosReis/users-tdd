7f34286dfb71460b04c374ea6064d53dc18ed17c
Marcus Reis <marcus.reis@askblue.pt>
2023-08-30 22:42:04 +0100

    The Increment Operator

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', likes: 0 })
+    joe = new User({ name: 'Joe', postCount: 0 })
     joe.save().then(() => done())
   })
 
@@ -25,7 +25,7 @@ describe('Updating records', () => {
   })
 
   it('A model instance can update', (done) => {
-    assertName(joe.updateOne({ name: 'Alex' }), done)
+    assertName(User.updateMany({ name: 'Joe' }, { name: 'Alex' }), done)
   })
 
   it('A model class can update', (done) => {
@@ -38,5 +38,14 @@ describe('Updating records', () => {
 
   it('A model class can find a record with an Id and update', (done) => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done)
+  })
+
+  it('A user can have their postcount incremented by 1', (done) => {
+    User.updateMany({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.postCount === 1)
+        done()
+      })
   })
 })

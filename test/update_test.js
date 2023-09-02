ba8c73b225cacf92098f6e147e9bcbd54544bc9d
Marcus Reis <marcus.reis@askblue.pt>
2023-09-02 12:54:42 +0100

    Fixing Postcount Update Tests

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0 })
+    joe = new User({ name: 'Joe', likes: 0 })
     joe.save().then(() => done())
   })
 
@@ -40,13 +40,13 @@ describe('Updating records', () => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done)
   })
 
-  //Once virtual types are enabled on userSchemma, set postCount is no longer required 
-  xit('A user can have their postcount incremented by 1', (done) => {
-    User.updateMany({ name: 'Joe' }, { $inc: { postCount: 1 } })
-      .then(() => User.findOne({ name: 'Joe' }))
-      .then((user) => {
-        assert(user.postCount === 1)
-        done()
-      })
+  it('A user can have their postcount incremented by 1', (done) => {
+    User.updateMany({ name: 'Joe' }, { $inc: { likes: 10 } })
+    .then(() => User.findOne({ name: 'Joe' }))
+    .then((user) => {
+      assert(user.likes === 10);
+      done();
+    });
   })
+
 })

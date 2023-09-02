bd9191ab935eca1e23ceab28ea8ccbecf806388f
Marcus Reis <marcus.reis@askblue.pt>
2023-09-02 12:42:51 +0100

    PostCount and Virtual Types

--- test/update_test.js
+++ test/update_test.js
@@ -40,7 +40,8 @@ describe('Updating records', () => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done)
   })
 
-  it('A user can have their postcount incremented by 1', (done) => {
+  //Once virtual types are enabled on userSchemma, set postCount is no longer required 
+  xit('A user can have their postcount incremented by 1', (done) => {
     User.updateMany({ name: 'Joe' }, { $inc: { postCount: 1 } })
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {

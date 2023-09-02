ecf4c1632bc4effbd71ff6907b5ae12b250b2ef3
Marcus Reis <marcus.reis@askblue.pt>
2023-09-02 04:59:04 +0100

    Validation With a Validator Function

--- test/validation_test.js
+++ test/validation_test.js
@@ -7,11 +7,18 @@ describe('Validating records', () => {
   it('requires a user name', () => {
     const user = new User({ name: undefined });
     const validationResult = user.validateSync();
-    console.log(validationResult)
+    //console.log(validationResult)
     const { message } = validationResult.errors.name;
 
     assert(message === 'Name is required.');
   });
 
+  it('requires a user\'s name longer than 2 characters', () => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name must be longer than 2 characters.');
+  });
 
 });
